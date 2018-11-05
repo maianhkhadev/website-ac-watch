dbfa308621f97238caa35c61e4d483f18d82e229
Mai Anh Kha <maianhkha.dev@gmail.com>
2018-11-05 15:34:52 +0700

    update

--- src/components/pages/home/section-hero.js
+++ src/components/pages/home/section-hero.js
@@ -8,7 +8,12 @@ function Section(props) {
     <section className="section-hero">
       <Slick>
         <div className="slick-item">
-          <div className="slide"></div>
+          <div className="slide">
+            <h2 className="slide-title">AC WATCH</h2>
+            <p>LOREM IPSUM DOLOR SIT AMET</p>
+            <button className="btn btn-outline-white">Read more</button>
+            <button className="btn btn-cream">Shop now</button>
+          </div>
         </div>
         <div className="slick-item">
           <div className="slide"></div>
@@ -17,6 +22,8 @@ function Section(props) {
           <div className="slide"></div>
         </div>
       </Slick>
+
+      <button className="btn btn-outline-white btn-scroll-down">Down</button>
     </section>
   )
 }
